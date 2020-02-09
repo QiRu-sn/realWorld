0e36c189ea158bf45625e17030854a4b6d4596ac
QiRu <916331669@qq.com>
2020-02-09 20:06:55 +0800

    项目初始化

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index-container">首页</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
