9d60506e4285c1b1639dcd6463076306c16734c8
QiRu <916331669@qq.com>
2020-02-09 21:13:25 +0800

    配置路由及页面布局

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,6 +3,12 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
+const _d9c58b38 = () => interopDefault(import('..\\pages\\editor.vue' /* webpackChunkName: "pages_editor" */))
+const _bd9b6e7c = () => interopDefault(import('..\\pages\\login.vue' /* webpackChunkName: "pages_login" */))
+const _58f200da = () => interopDefault(import('..\\pages\\register.vue' /* webpackChunkName: "pages_register" */))
+const _42d4634c = () => interopDefault(import('..\\pages\\settings.vue' /* webpackChunkName: "pages_settings" */))
+const _e788ce6a = () => interopDefault(import('..\\pages\\article\\_slug.vue' /* webpackChunkName: "pages_article__slug" */))
+const _8469673a = () => interopDefault(import('..\\pages\\profile\\_username.vue' /* webpackChunkName: "pages_profile__username" */))
 const _50eab4aa = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
 
 // TODO: remove in Nuxt 3
@@ -22,6 +28,30 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/editor",
+    component: _d9c58b38,
+    name: "editor"
+  }, {
+    path: "/login",
+    component: _bd9b6e7c,
+    name: "login"
+  }, {
+    path: "/register",
+    component: _58f200da,
+    name: "register"
+  }, {
+    path: "/settings",
+    component: _42d4634c,
+    name: "settings"
+  }, {
+    path: "/article/:slug?",
+    component: _e788ce6a,
+    name: "article-slug"
+  }, {
+    path: "/profile/:username?",
+    component: _8469673a,
+    name: "profile-username"
+  }, {
     path: "/",
     component: _50eab4aa,
     name: "index"
